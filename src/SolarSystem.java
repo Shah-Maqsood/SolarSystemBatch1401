764af7674f286ca556aa511b7125e59b7e811cab
Shah-Maqsood <koelnbonn99@gmail.com>
2022-12-18 17:44:53 +0100

    Version first so far

---
+++ src/SolarSystem.java
@@ -0,0 +1,12 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+
+
+        SolarSystem Jinny=new SolarSystem();
+
+        int sun;
+        int Moon;
+        int Planet;
+
+    }
+}
